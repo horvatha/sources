0a779f94ffe05ad721defd088cdb5886fd587ab3
Arpad Horvath <horvath.arpad.szfvar@gmail.com>
2014-09-21 09:12:43 +0200

    Some tests added

--- tests.py
+++ tests.py
@@ -1,3 +1,16 @@
+from django.core.urlresolvers import resolve
+from django.template.loader import render_to_string
+from django.http import HttpRequest
 from django.test import TestCase
+from sources.views import home
 
-# Create your tests here.
+
+class SourceHomePageTest(TestCase):
+    def test_source_url_resolves_home_page_view(self):
+        found = resolve("/sources/")
+        self.assertEqual(found.func, home)
+
+    def test_home_page_returns_correct_html(self):
+        response = home(HttpRequest())
+        expected_html = render_to_string('sources/sourcestat.html')
+        self.assertEqual(response.content.decode(), expected_html)
