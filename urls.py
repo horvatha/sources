e18cd003b5cdcf0e0eaf3b81338a7753aec31fb0
Arpad Horvath <horvath.arpad.szfvar@gmail.com>
2014-09-21 09:47:42 +0200

    Some small modification

--- urls.py
+++ urls.py
@@ -3,6 +3,8 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns(
     '',
     url(r'^$', 'sources.views.home', name='source_home'),
-    url(r'^(\d+)/$', 'sources.views.sourcestat_detail',
-        name='sourcestat_detail'),
+    url(r'^(\d+)/$', 'sources.views.source_detail',
+        name='source_detail'),
+    url(r'^(\d+)/default/$', 'sources.views.sourcestat_default',
+        name='sourcestat_default')
 )
--- views.py
+++ views.py
@@ -1,14 +1,28 @@
 from django.shortcuts import render
 import coding
 
+sources = {
+    1: (coding.Source([.25]*4), coding.Code("00 01 10 11")),
+}
+
 
 def home(request):
-    return render(request, "djancoding/sourcestat.html", {})
+    return render(request, "sources/sourcestat.html", {})
 
 
-def sourcestat_detail(request, id):
-    sources = {
-        1: coding.Source([.25]*4)
-    }
-    source = sources[int(id)]
-    return render(request, "djancoding/sourcestat_detail.html", {"source": source})
+def source_detail(request, id):
+    source, _ = sources[int(id)]
+    return render(
+        request,
+        "sources/sourcestat_detail.html",
+        {"source": source}
+    )
+
+
+def sourcestat_default(request, id):
+    source, code = sources[int(id)]
+    return render(
+        request,
+        "sources/sourcestat_detail.html",
+        {"source": source, "code": code}
+    )
